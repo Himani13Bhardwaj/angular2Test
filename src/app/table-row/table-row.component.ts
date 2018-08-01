3062a0624f8e1fb5f5b3d96a09bb8991ad9f647c
Himani13Bhardwaj <hrabhardwaj@gmail.com>
2018-08-01 19:40:57 +0530

    Add files via upload

--- src/app/table-row/table-row.component.ts
+++ src/app/table-row/table-row.component.ts
@@ -1,1 +1,23 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-table-row',
+  templateUrl: './table-row.component.html',
+  styleUrls: ['./table-row.component.css']
+})
+export class TableRowComponent implements OnInit {
+  row:any;
+  constructor() { 
+    this.row = {
+      id: 0,
+      username: '',
+      phone: '',
+      role: '',
+      name: ''
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
